05a036b18711d3e59076ecb6613ea06b24d9599d
Sumer Patel <patelsum@oregonstate.edu>
2023-09-23 19:36:11 -0700

    Setup UI for Initial Modal

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+// React packages
+import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+
+// Zod library
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+// Components
+import { 
+  Dialog, 
+  DialogContent, 
+  DialogDescription, 
+  DialogFooter, 
+  DialogHeader, 
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { 
+  FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required."
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required."
+  })
+})
+
+export const InitialModal = () => {
+  const [ isMounted, setIsMounted ] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    }
+  })
+
+  // Custom loader
+  const isLoading  = form.formState.isSubmitting
+
+  // Custom form submit
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <Dialog open={true}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can always change it later
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                ToDo: Image Upload
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-grey-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
