4e4375dbde65c6319a97b8bd965b09fc2bea26ef
Sumer Patel <patelsum@oregonstate.edu>
2023-09-25 02:10:22 -0700

    Implemented Server Modal

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -3,15 +3,23 @@
 // React packages
 import { Plus } from "lucide-react"
 
+// Custom hook
+import { useModal } from "@/hooks/use-model-store"
+
 // Components
 import { ActionTooltip } from "@/components/action-tooltip"
 
 // Action button in navbar
 export const NavigationAction = () => {
+  const { onOpen } = useModal()
+
   return (
     <div>
       <ActionTooltip side="right" align="center" label="Add a server">
-        <button className="group flex items-center">
+        <button 
+          onClick={() => onOpen("createServer")}
+          className="group flex items-center"
+        >
           <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[18px] 
             transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 
           group-hover:bg-emerald-500"
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+// React packages
+import { useEffect, useState } from "react"
+
+// Components
+import { CreateServerModal } from "@/components/modals/create-server-modal"
+
+// This is return a Fragment and here we will render all our modals
+export const ModalProvider = () => {
+  // Preventing the modals to be rendered on server side coz that can cause inconsistency, thus creating Hydration errors.
+  const [ isMounted, setIsMounted ] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <>
+      <CreateServerModal />
+    </>
+  )
+}
