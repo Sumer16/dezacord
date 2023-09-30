a82548d6f508590938ffa231a10cdd768b691402
Sumer Patel <patelsum@oregonstate.edu>
2023-09-29 21:50:38 -0700

    Fixed Image related warning

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -40,6 +40,7 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
             fill
             src={imageUrl}
             alt="Channel"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
       </button>
