000acf4dcc6873399c61f40a17e4b8ee9fd2962d
Sumer Patel <patelsum@oregonstate.edu>
2023-10-06 22:54:49 -0700

    Added Chat Header & Menu toggle button

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,58 @@
-const ChannelIdPage = () => {
+// NextJS packages
+import { redirect } from "next/navigation"
+
+// Clerk
+import { redirectToSignIn } from "@clerk/nextjs"
+
+// Lib files
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+
+// Components
+import { ChatHeader } from "@/components/chat/chat-header"
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+
+const ChannelIdPage = async ({ 
+  params 
+}: ChannelIdPageProps) => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  // To find the channel ID
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  })
+  
+  // To find the member using serverId and profileId as members already inside would have the server Id and also the profile Id associated to that server
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  })
+
+  if (!channel || !member) {
+    redirect("/")
+  }
+
   return (
-    <div>
-      Channel ID page
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader 
+        serverId={channel.serverId} 
+        name={channel.name} 
+        type="channel" 
+      />
     </div>
   )
 }
