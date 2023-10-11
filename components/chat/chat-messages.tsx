8ff470157d4726370b9f778d11ceae546550559c
Sumer Patel <patelsum@oregonstate.edu>
2023-10-11 00:42:42 -0700

    Implemented chat item component to display messages with actions

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -8,12 +8,19 @@ import { useChatQuery } from "@/hooks/use-chat-query"
 
 // Folder Components
 import { ChatWelcome } from "./chat-welcome"
+import { ChatItem } from "./chat-item"
+
+// Libraries
+import { format } from "date-fns"
 
 // Prisma packages
 import { Member, Message, Profile } from "@prisma/client"
 
 // Icons
 import { Loader2, ServerCrash } from "lucide-react"
+
+// Date format using the date-fns library for the timestamp
+const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
 // Custom type to include Member, Message and Profile
 type MessageWithMemberWithProfile = Message & {
@@ -93,9 +100,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>
-                {message.content}
-              </div>
+              <ChatItem 
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
