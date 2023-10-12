e6944e48369b80807780964250cdd0beeedbce34
Sumer Patel <patelsum@oregonstate.edu>
2023-10-12 14:50:45 -0700

    Removed socketIO routes from middleware

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from "@clerk/nextjs"
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing"],
+  ignoredRoutes: ["/api/socket/io"]
 })
 
 export const config = {
